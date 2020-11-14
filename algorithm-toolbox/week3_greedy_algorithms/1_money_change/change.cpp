eacd3265377df133f8d2fba78c4f3c5d9a408bca
Gilmar Lima <gilmar.mail@gmail.com>
2020-11-14 16:28:53 -0300

    change

--- algorithm-toolbox/week3_greedy_algorithms/1_money_change/change.cpp
+++ algorithm-toolbox/week3_greedy_algorithms/1_money_change/change.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
+#include <vector>
+#include <numeric>
+
+using namespace std;
 
 int get_change(int m) {
-  //write your code here
-  return n;
+
+    const int value [3] = {10,5,1};
+    vector<int> number_of_coins;
+    int division_result;
+    
+    for(int i = 0; i <= 2; i++){
+
+      division_result = m / value[i];
+      m = m % value[i];
+      
+      number_of_coins.push_back( division_result ); 
+    }
+    
+    return accumulate(number_of_coins.begin(), number_of_coins.end(),0);
 }
 
 int main() {
-  int m;
-  std::cin >> m;
-  std::cout << get_change(m) << '\n';
+    int m;
+    cin >> m;
+    cout << get_change(m) << '\n';
 }
