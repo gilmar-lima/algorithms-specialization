1b26e2090efcbfe25742cc6811cd363adba6295c
Gilmar Lima <gilmar.mail@gmail.com>
2020-11-15 01:17:15 -0300

    knapsack

--- algorithm-toolbox/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
+++ algorithm-toolbox/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
@@ -1,29 +1,51 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
-using std::vector;
+using namespace std;
 
 double get_optimal_value(int capacity, vector<int> weights, vector<int> values) {
-  double value = 0.0;
+    double value = 0.0;
+    int size = values.size();
+    int max_element_index = 0;
+    int weight;
+    vector<double> relative_values(size);
+  
+    for(int i = 0; i < size; i++){
+	relative_values[i] = (double) values[i]/weights[i];    
+    }
 
-  // write your code here
+    for(int i = 0; i < size; i++){
+	if(capacity == 0)
+	    return value;
 
-  return value;
+	max_element_index =
+	    max_element(relative_values.begin(), relative_values.end())
+	    - relative_values.begin();
+
+	weight = min(weights[max_element_index], capacity);
+	value = value + (double) weight*relative_values[max_element_index];
+	capacity = capacity - weight;
+	relative_values[max_element_index] = 0.0;
+	
+    }
+    
+    return value;
 }
 
 int main() {
-  int n;
-  int capacity;
-  std::cin >> n >> capacity;
-  vector<int> values(n);
-  vector<int> weights(n);
-  for (int i = 0; i < n; i++) {
-    std::cin >> values[i] >> weights[i];
-  }
+    int n;
+    int capacity;
+    cin >> n >> capacity;
+    vector<int> values(n);
+    vector<int> weights(n);
+    for (int i = 0; i < n; i++) {
+	cin >> values[i] >> weights[i];
+    }
 
-  double optimal_value = get_optimal_value(capacity, weights, values);
+    double optimal_value = get_optimal_value(capacity, weights, values);
 
-  std::cout.precision(10);
-  std::cout << optimal_value << std::endl;
-  return 0;
+    cout.precision(10);
+    cout << optimal_value << endl;
+    return 0;
 }
