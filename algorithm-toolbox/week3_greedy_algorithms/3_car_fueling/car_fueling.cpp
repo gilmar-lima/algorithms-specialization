461a89b5b1054d647778d331c399946ec8d8a3c6
Gilmar Lima <gilmar.mail@gmail.com>
2020-11-15 14:28:37 -0300

    car refuel challenge

--- algorithm-toolbox/week3_greedy_algorithms/3_car_fueling/car_fueling.cpp
+++ algorithm-toolbox/week3_greedy_algorithms/3_car_fueling/car_fueling.cpp
@@ -1,14 +1,35 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
-using std::cin;
-using std::cout;
-using std::vector;
-using std::max;
+using namespace std;
 
 int compute_min_refills(int dist, int tank, vector<int> & stops) {
-    // write your code here
-    return -1;
+    
+    stops.push_back(dist);
+    stops.push_back(0);
+    sort(stops.begin(), stops.end());
+   
+    int num_refils = 0;
+    int current_refil = 0;
+    int last_refil = 0;
+    int num_stops = stops.size();
+    
+    while (current_refil < num_stops - 1) {
+	last_refil = current_refil;
+
+	while ((current_refil + 1 < num_stops) &&
+	       (stops[current_refil + 1] - stops[last_refil] <= tank)) {
+	    current_refil++;	    
+	}
+
+	if(current_refil == last_refil)
+	    return -1;
+
+	if(current_refil < num_stops - 1)
+	    num_refils++;
+    }
+    return num_refils;
 }
 
 
