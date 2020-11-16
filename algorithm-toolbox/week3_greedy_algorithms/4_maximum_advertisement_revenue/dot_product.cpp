c89cd3038936c7559ec88f71a537f443c5cf88ac
Gilmar Lima <gilmar.mail@gmail.com>
2020-11-16 18:17:06 -0300

    dot product

--- algorithm-toolbox/week3_greedy_algorithms/4_maximum_advertisement_revenue/dot_product.cpp
+++ algorithm-toolbox/week3_greedy_algorithms/4_maximum_advertisement_revenue/dot_product.cpp
@@ -2,26 +2,48 @@
 #include <iostream>
 #include <vector>
 
-using std::vector;
+using namespace std;
 
 long long max_dot_product(vector<int> a, vector<int> b) {
-  // write your code here
-  long long result = 0;
-  for (size_t i = 0; i < a.size(); i++) {
-    result += ((long long) a[i]) * b[i];
-  }
-  return result;
+
+    long long result = 0;    
+    int temp_i, temp_j;
+    long long temp_value, mult;
+    int size = a.size();
+
+    for (int n = 0; n < size; n++) {
+	
+	temp_value = (long long) a[0]*b[0];
+
+	for (size_t i = 0; i < a.size(); i++){
+	    for (size_t j = 0; j < b.size(); j++){
+		mult = (long long) a[i]*b[j];
+
+		if(mult >= temp_value){
+		    temp_value = mult;
+		    temp_i = i;
+		    temp_j = j;
+		}		
+	    }
+	}
+
+	result = result + temp_value;
+	a.erase(a.begin() + temp_i);
+	b.erase(b.begin() + temp_j);
+    }
+
+    return result;
 }
 
 int main() {
-  size_t n;
-  std::cin >> n;
-  vector<int> a(n), b(n);
-  for (size_t i = 0; i < n; i++) {
-    std::cin >> a[i];
-  }
-  for (size_t i = 0; i < n; i++) {
-    std::cin >> b[i];
-  }
-  std::cout << max_dot_product(a, b) << std::endl;
+    size_t n;
+    std::cin >> n;
+    vector<int> a(n), b(n);
+    for (size_t i = 0; i < n; i++) {
+	std::cin >> a[i];
+    }
+    for (size_t i = 0; i < n; i++) {
+	std::cin >> b[i];
+    }
+    std::cout << max_dot_product(a, b) << std::endl;
 }
