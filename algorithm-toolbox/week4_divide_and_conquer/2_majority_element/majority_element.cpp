2ad1126d29b42af45480253ee66a778ce618844a
Gilmar Lima <gilmar.mail@gmail.com>
2020-11-25 11:11:48 -0300

    majority element

--- algorithm-toolbox/week4_divide_and_conquer/2_majority_element/majority_element.cpp
+++ algorithm-toolbox/week4_divide_and_conquer/2_majority_element/majority_element.cpp
@@ -4,73 +4,38 @@
 
 using namespace std;
 
-void move_rest(vector<int> &destination, int i_destination, vector<int> &source, int i_source){
-
-    while (i_destination < destination.size()){
-	destination[i_destination] = source[i_source];
-	i_destination++;
-	i_source++
-    }
-}
-
-
-vector<int> merge(vector<int> b, vector<int> c){
-    vector<int> d (b.size() + c.size(), 0);
-    int i_c = 0, i_b = 0, i_d = 0;
-
-    while ((i_c <= c.size()-1) && (i_b <= b.size()-1) ) {
-
-	if(b[i_b] <= c[i_c]){
-
-	    d[i_d] = b[i_b];
-	    i_b++;
-	    i_d++;
-	    
-	}else{
-
-	    d[i_d] = c[i_c];
-	    i_c++;
-	    i_d++;
-	    
-	}
-    }
-
-    if(i_c <= c.size()-1){
-	move_rest(d, i_d, c, i_c);
-    }else if(i_b <= b.size()-1){
-	move_rest(d, i_d, b, i_b);
-    }
-
-    return d;
-}
-
-vector<int> merge_sort(vector<int> &a, int left, int right){
-
-    if(right == 1){
-	return a;
-    }
-    int mid = right/2;
-    vector<int> b = merge_sort(a, left, mid);
-    vector<int> c = merge_sort(a, mid + 1, right);
-    vector<int> a_prime = merge(b, c);
-
-    return a_prime;   
-}
-
 int get_majority_element(vector<int> &a, int left, int right) {
-  if (left == right) return -1;
-  if (left + 1 == right) return a[left];
-  
-  
-  return -1;
+    if (left == right) return -1;
+    if (left + 1 == right) return a[left];
+
+    sort(a.begin(), a.end());
+
+    int current_element = a[left];
+    int counter = 0;
+    int half = right/2;
+    
+    while(left < right){
+
+	if(current_element == a[left]){
+	    counter++;
+	    if(counter > half) return current_element;
+	}else{
+	    current_element = a[left];
+	    counter = 1;	    
+	}
+
+	left++;	    
+    }
+      
+    return -1;
 }
 
 int main() {
-  int n;
-  std::cin >> n;
-  vector<int> a(n);
-  for (size_t i = 0; i < a.size(); ++i) {
-    std::cin >> a[i];
-  }
-  std::cout << (get_majority_element(a, 0, a.size()) != -1) << '\n';
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (size_t i = 0; i < a.size(); ++i) {
+	cin >> a[i];
+    }
+    cout << (get_majority_element(a, 0, a.size()) != -1) << '\n';
 }
