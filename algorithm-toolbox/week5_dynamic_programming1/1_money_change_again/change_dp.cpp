dc3942faebc0cae852c423c65ca12f81db0cf919
Gilmar Lima <gilmar.mail@gmail.com>
2020-12-01 16:41:52 -0300

    change

--- algorithm-toolbox/week5_dynamic_programming1/1_money_change_again/change_dp.cpp
+++ algorithm-toolbox/week5_dynamic_programming1/1_money_change_again/change_dp.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
+#include <vector>
 
-int get_change(int m) {
-  //write your code here
-  return m / 4;
+using namespace std;
+
+int get_change(int money) {
+    
+    vector<int> coins = {1,3,4};
+    vector<int> min_num_coins (money + 1,0);
+    int num_coins = 0;
+    
+    for(int m = 1; m <= money; m++){
+	min_num_coins[m] = money + 1;
+
+	for(int coin : coins){
+	    if (m >= coin){
+		num_coins = min_num_coins[m - coin] + 1;
+		if (num_coins < min_num_coins[m]){
+		    min_num_coins[m] = num_coins;
+		}
+	    }	    
+	}
+
+    }
+        
+    return min_num_coins[money];
 }
 
 int main() {
-  int m;
-  std::cin >> m;
-  std::cout << get_change(m) << '\n';
+    int m;
+    std::cin >> m;
+    std::cout << get_change(m) << '\n';
 }
