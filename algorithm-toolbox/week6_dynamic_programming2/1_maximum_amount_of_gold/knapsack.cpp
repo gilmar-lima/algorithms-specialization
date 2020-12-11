a59d66e358ddb0deb322b1bc5fd76b6112a1fe73
Gilmar Lima <gilmar.mail@gmail.com>
2020-12-11 07:52:33 -0300

    knapstack

--- algorithm-toolbox/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.cpp
+++ algorithm-toolbox/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.cpp
@@ -1,25 +1,40 @@
 #include <iostream>
 #include <vector>
 
-using std::vector;
+using namespace std;
 
 int optimal_weight(int W, const vector<int> &w) {
-  //write your code here
-  int current_weight = 0;
-  for (size_t i = 0; i < w.size(); ++i) {
-    if (current_weight + w[i] <= W) {
-      current_weight += w[i];
+  
+    int n = w.size();
+    vector<int> v = w;
+    vector<vector<int>> value(n+1, vector<int>( W+1, 0));
+ 
+    for (int j = 0; j <= W; j++)
+      value[0][j] = 0;
+ 
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j <= W; j++)
+	{
+            if (w[i-1] > j)
+	      value[i][j] = value[i-1][j];
+            else
+	      value[i][j] = max(value[i-1][j],
+                            value[i-1][j-w[i-1]] + v[i-1]);
+	}
     }
-  }
-  return current_weight;
+ 
+
+    return value[n][W];
+    
 }
 
 int main() {
-  int n, W;
-  std::cin >> W >> n;
-  vector<int> w(n);
-  for (int i = 0; i < n; i++) {
-    std::cin >> w[i];
-  }
-  std::cout << optimal_weight(W, w) << '\n';
+    int n, W;
+    std::cin >> W >> n;
+    vector<int> w(n);
+    for (int i = 0; i < n; i++) {
+	std::cin >> w[i];
+    }
+    std::cout << optimal_weight(W, w) << '\n';
 }
