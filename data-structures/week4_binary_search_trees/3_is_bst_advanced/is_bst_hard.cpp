55ea63dc9ee0f2d94ba04c05227f365cc561a1ca
Gilmar Lima <gilmar.mail@gmail.com>
2021-01-23 10:21:13 -0300

    bst hard

--- data-structures/week4_binary_search_trees/3_is_bst_advanced/is_bst_hard.cpp
+++ data-structures/week4_binary_search_trees/3_is_bst_advanced/is_bst_hard.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <cstddef>
+#include <climits>
 
 using namespace std;
 
@@ -14,13 +15,26 @@ struct Node {
   Node(int key_, int left_, int right_) : key(key_), left(left_), right(right_) {}
 };
 
-bool check_node(int node_index, const vector<Node>& tree){
+struct Boundaries
+{
+  int min;
+  int max;
+
+  Boundaries(int min_, int max_) : min(min_), max(max_) {}
+};
+
+
+bool check_node(int node_index, const vector<Node>& tree, Boundaries node_limits){
 
     if(node_index == -1){
       return true;
     }
-    
 
+    //check wether the current node respect the limits of its branch
+    if(tree[node_index].key > node_limits.max) return false;
+    if(tree[node_index].key < node_limits.min) return false;
+
+    //check if the current node children respect the rules
     if(tree[node_index].left == -1 && tree[node_index].right == -1){
       return true;
     }else if(tree[node_index].left == -1 && tree[node_index].right != -1){
@@ -29,19 +43,29 @@ bool check_node(int node_index, const vector<Node>& tree){
       if(tree[tree[node_index].left].key >= tree[node_index].key) return false;
     }else{
       if(tree[node_index].key > tree[tree[node_index].right].key || tree[tree[node_index].left].key >= tree[node_index].key) return false;
-    }  
+    }
 
-    return (check_node(tree[node_index].left, tree) && check_node(tree[node_index].right, tree));
+    Boundaries left_limits = node_limits;
+    Boundaries right_limits = node_limits;
+
+    if(tree[node_index].key > node_limits.min){
+      right_limits.min = tree[node_index].key;
+    }
+
+    if(tree[node_index].key < node_limits.max){
+      left_limits.max = tree[node_index].key;
+    }     
+
+    return (check_node(tree[node_index].left, tree, left_limits) && check_node(tree[node_index].right, tree, right_limits));
 
 }
 
 bool IsBinarySearchTree(const vector<Node>& tree) {
-  // Implement correct algorithm here
-  
-
   if(tree.size() == 0) return true; 
 
-  return check_node(0,tree);
+  Boundaries node_limits(INT_MIN, INT_MAX) ;
+
+  return check_node(0,tree, node_limits);
 }
 
 int main() {
