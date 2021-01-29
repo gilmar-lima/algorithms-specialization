a32bd96d6725fd505acb5a4086ae6a9d989df4e9
Gilmar Lima <gilmar.mail@gmail.com>
2021-01-28 23:25:23 -0300

    reach

--- algorithms-graphs/week1_graph_decomposition1/1_finding_exit_from_maze/reachability.cpp
+++ algorithms-graphs/week1_graph_decomposition1/1_finding_exit_from_maze/reachability.cpp
@@ -1,26 +1,41 @@
 #include <iostream>
 #include <vector>
 
-using std::vector;
-using std::pair;
+using namespace std;
 
+void explore(vector<bool> &visited, vector<vector<int> > &adj, const int &x){
+
+  visited[x] = true;
+
+  for (auto w : adj[x])
+  {
+    if(!visited[w]){
+      explore(visited,adj,w);
+    }
+  }
+}
 
 int reach(vector<vector<int> > &adj, int x, int y) {
-  //write your code here
+  vector<bool> visited (adj.size(), false);
+  
+  explore(visited,adj,x);
+
+  if(visited[y]) return 1;
+
   return 0;
 }
 
 int main() {
   size_t n, m;
-  std::cin >> n >> m;
+  cin >> n >> m;
   vector<vector<int> > adj(n, vector<int>());
   for (size_t i = 0; i < m; i++) {
     int x, y;
-    std::cin >> x >> y;
+    cin >> x >> y;
     adj[x - 1].push_back(y - 1);
     adj[y - 1].push_back(x - 1);
   }
   int x, y;
-  std::cin >> x >> y;
-  std::cout << reach(adj, x - 1, y - 1);
+  cin >> x >> y;
+  cout << reach(adj, x - 1, y - 1);
 }
