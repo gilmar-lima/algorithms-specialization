a574aa8f626639233b6bae1955c96b81fb85996c
Gilmar Lima <gilmar.mail@gmail.com>
2021-02-06 16:24:25 -0300

    acyclicity

--- algorithms-graphs/week2_graph_decomposition2/1_cs_curriculum/acyclicity.cpp
+++ algorithms-graphs/week2_graph_decomposition2/1_cs_curriculum/acyclicity.cpp
@@ -1,12 +1,69 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
-using std::vector;
-using std::pair;
+using namespace std;
+
+int counter = 1;
+int cyclicity = 0;
+unordered_map<int,int> post;
+
+void post_visit(int v){
+  post[v] = counter;
+  counter++;
+}
+
+void explore(vector<bool> &visited, vector<vector<int> > &adj, const int &v){
+
+  visited[v] = true;
+  for (auto w : adj[v])
+  {
+    if(!visited[w]){
+      explore(visited,adj,w);      
+    }
+  }
+  post_visit(v);
+}
+
+void explore2(vector<bool> &visited, vector<vector<int> > &adj, const int &v){
+  visited[v] = true;
+  for (auto w : adj[v])
+  {
+    if(!visited[w]){
+      if(post[v]>post[w]){
+        cyclicity = 1;        
+      } 
+      explore2(visited,adj,w);      
+    }
+  }
+}
+
+void DFS(vector<vector<int> > &adj){
+  vector<bool> visited (adj.size(), false);
+
+  for (size_t v = 0; v < adj.size(); v++)
+  {
+    if(!visited[v]){
+      explore(visited,adj,v); 
+    }
+  } 
+}
+
+void DFS2(vector<vector<int> > &adj){
+  vector<bool> visited (adj.size(), false);
+
+  for (size_t v = 0; v < adj.size(); v++)
+  {
+    if(!visited[v]){
+      explore2(visited,adj,v); 
+    }
+  } 
+}
 
 int acyclic(vector<vector<int> > &adj) {
-  //write your code here
-  return 0;
+  DFS(adj);
+  //DFS2(adj);
+  return cyclicity;
 }
 
 int main() {
