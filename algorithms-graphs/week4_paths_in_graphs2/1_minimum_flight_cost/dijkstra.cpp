62deef8e716b0a50014ae9c9470fb083f428114e
Gilmar Lima <gilmar.mail@gmail.com>
2021-02-19 18:27:42 -0300

    dijkstra

--- algorithms-graphs/week4_paths_in_graphs2/1_minimum_flight_cost/dijkstra.cpp
+++ algorithms-graphs/week4_paths_in_graphs2/1_minimum_flight_cost/dijkstra.cpp
@@ -1,15 +1,53 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <unordered_set>
+#include <climits>
 
-using std::vector;
-using std::queue;
-using std::pair;
-using std::priority_queue;
+using namespace std;
+
+struct node{
+  int value;
+  int distance;
+};
 
 long long distance(vector<vector<int> > &adj, vector<vector<int> > &cost, int s, int t) {
-  //write your code her
-  return -1;
+  unordered_set<int> R;
+  vector<int> dist (adj.size(), INT_MAX);
+  vector<int> prev (adj.size(), -1);
+
+  auto comparison = [](node a, node b){
+    return a.distance > b.distance;          
+  };
+  priority_queue<node, vector<node>, decltype(comparison)> V (comparison);
+
+  dist[s] = 0;
+  node s_node {s,0};
+  V.push(s_node);
+  
+  while(V.size() > 0){
+    node v =  V.top();
+    unordered_set<int>::const_iterator item = R.find(v.value);
+    if(item == R.end()){   
+
+      R.insert(v.value);
+      for(size_t i = 0; i < adj[v.value].size(); i++){
+        int z = adj[v.value][i];
+        if (dist[z] > (dist[v.value] + cost[v.value][i])){
+          dist[z] = dist[v.value] + cost[v.value][i];
+          prev[z] = v.value;
+
+          node z_node {z,dist[z]};
+          V.push(z_node);
+        }
+      }
+
+    }
+
+    V.pop();    
+  }
+
+  return dist[t] == INT_MAX ? -1 : dist[t];
 }
 
 int main() {
