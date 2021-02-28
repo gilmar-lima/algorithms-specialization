51f8849a00fe5a5b841b8f90743e07e98f580719
Gilmar Lima <gilmar.mail@gmail.com>
2021-02-28 02:43:46 -0300

    first attempt

--- algorithms-graphs/week5_spanning_trees/1_connecting_points/connecting_points.cpp
+++ algorithms-graphs/week5_spanning_trees/1_connecting_points/connecting_points.cpp
@@ -2,14 +2,92 @@
 #include <iostream>
 #include <iomanip>
 #include <vector>
+#include <queue>
 #include <cmath>
+#include <unordered_set>
 
-using std::vector;
+using namespace std;
+
+struct point{
+  int value;
+  int x,y;
+  int prev;
+  double cost;
+
+  point(int x, int y) : x(x), y(y) {
+    prev = -1;
+    cost = INFINITY;
+  }
+
+  point(){
+    prev = -1;
+    cost = INFINITY;
+  }
+};
+
+double distance(point a, point b){
+  double result = (double) sqrt (pow (a.x - b.x, 2) + pow (a.y - b.y, 2));
+  return result;
+}
+
+double total_length(const vector<point> &V){
+  double result = 0.0;
+  for (auto &&i : V)
+  {
+    result += i.cost;
+  }
+  return result;  
+}
+
+void fill_coordinates(vector<int> &x, vector<int> &y, vector<point> &V){
+  for (size_t i = 0; i < x.size(); i++)
+  {
+    V[i].value = i;
+    V[i].x = x[i];
+    V[i].y = y[i];
+  }  
+}
 
 double minimum_distance(vector<int> x, vector<int> y) {
-  double result = 0.;
-  //write your code here
-  return result;
+  double result = 0.0;
+  
+  auto comparison = [](point a, point b){
+    return a.cost > b.cost;          
+  };
+  priority_queue<point, vector<point>, decltype(comparison)> H (comparison);
+  unordered_set<int> R;
+  vector<point> V (x.size());
+  fill_coordinates(x,y,V);
+
+  V[0].cost = 0.0;
+  H.push(V[0]);
+  
+  while(H.size() > 0){
+    point v = H.top();
+    H.pop();    
+    unordered_set<int>::const_iterator item = R.find(v.value);
+    
+    R.insert(v.value);
+
+    //vertex already explored
+    if(item != R.end()) continue;
+
+    for (size_t i = 0; i < V.size() ; i++)
+    {
+      //skip reference to self vertex
+      if( i == v.value) continue;
+      point* z = &V[i];
+      double weight_v_z = distance(v,*z);
+
+      if (z->cost > weight_v_z){
+        z->cost = weight_v_z;
+        z->prev = i;
+        H.push(*z);
+      }
+    }
+  }
+
+  return total_length(V);
 } 
 
 int main() {
