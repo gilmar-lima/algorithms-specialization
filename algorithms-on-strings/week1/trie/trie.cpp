5a15b0116db4eddbfb5a82e5849f1dbf993f026f
Gilmar Lima <gilmar.mail@gmail.com>
2021-03-12 18:51:24 -0300

    trie

--- algorithms-on-strings/week1/trie/trie.cpp
+++ algorithms-on-strings/week1/trie/trie.cpp
@@ -12,7 +12,30 @@ typedef vector<edges> trie;
 
 trie build_trie(vector<string> & patterns) {
   trie t;
-  // write your code here
+  edges new_node;
+  t.push_back(new_node);
+  
+
+  auto have_symbol = [](const trie& t, const int& current_node, const char& current_symbol){
+    return t[current_node].find(current_symbol) != t[current_node].end();
+  };
+
+  for (auto &&pattern : patterns)
+  {
+    int current_node = 0;
+    for (size_t i = 0; i < pattern.size(); i++)
+    {
+      char current_symbol = pattern[i];
+      if(have_symbol(t,current_node,current_symbol)){
+        current_node = t[current_node].find(current_symbol)->second;
+      }else{
+        t.push_back(new_node);
+        int new_node_position = t.size() -1;
+        t[current_node].insert(std::pair<char,int>(current_symbol,new_node_position));
+        current_node = new_node_position;
+      }
+    }    
+  }
   return t;
 }
 
