cb5b5c74348e57b9e96a3d3490d52891156339d7
Gilmar Lima <gilmar.mail@gmail.com>
2021-03-14 01:42:20 -0300

    trie matching

--- algorithms-on-strings/week1/trie_matching/trie_matching.cpp
+++ algorithms-on-strings/week1/trie_matching/trie_matching.cpp
@@ -4,51 +4,94 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 
 using namespace std;
 
-int const Letters =    4;
-int const NA      =   -1;
+typedef map<char, int> edges;
+typedef vector<edges> trie;
 
-struct Node
-{
-	int next [Letters];
+trie build_trie(const vector<string>& patterns) {
+  trie t;
+  edges new_node;
+  t.push_back(new_node);
+  
 
-	Node ()
-	{
-		fill (next, next + Letters, NA);
+  auto have_symbol = [](const trie& t, const int& current_node, const char& current_symbol){
+    return t[current_node].find(current_symbol) != t[current_node].end();
+  };
+
+  for (auto &&pattern : patterns)
+  {
+    int current_node = 0;
+    for (size_t i = 0; i < pattern.size(); i++)
+    {
+      char current_symbol = pattern[i];
+      if(have_symbol(t,current_node,current_symbol)){
+        current_node = t[current_node].find(current_symbol)->second;
+      }else{
+        t.push_back(new_node);
+        int new_node_position = t.size() -1;
+        t[current_node].insert(std::pair<char,int>(current_symbol,new_node_position));
+        current_node = new_node_position;
+      }
+    }    
+  }
+  return t;
+}
+
+bool prefix_trie_matching(const string& text, const trie& t){
+	int symbol_position = 0;
+	char symbol = text[symbol_position];
+	int v = 0;
+
+	auto is_leaf = [](const int& v, const trie& t){
+		return t[v].size() == 0;
+	};
+
+	auto have_edge = [](const int& v, const trie& t,const char& symbol){
+		return t[v].find(symbol) != t[v].end();
+	};
+
+	while(true){
+		if(is_leaf(v,t)){
+			return true;
+		}else if(have_edge(v,t,symbol)){
+			v = t[v].find(symbol)->second;
+			symbol_position++;
+			symbol = text[symbol_position];
+		}else{
+			return false;
+		}
+	}
+}
+
+vector<int> trie_matching(const string& text, const trie& t){
+	int text_position = 0;
+	vector<int> match_position;
+
+	while(text_position < text.size()){
+		bool match = prefix_trie_matching(text.substr(text_position,text.size()),t);
+		if(match) match_position.push_back(text_position);
+		text_position++;
 	}
 
-	bool isLeaf () const
-	{
-	    return (next[0] == NA && next[1] == NA && next[2] == NA && next[3] == NA);
-	}
-};
-
-int letterToIndex (char letter)
-{
-	switch (letter)
-	{
-		case 'A': return 0; break;
-		case 'C': return 1; break;
-		case 'G': return 2; break;
-		case 'T': return 3; break;
-		default: assert (false); return -1;
-	}
+	return match_position;
 }
 
 vector <int> solve (const string& text, int n, const vector <string>& patterns)
 {
 	vector <int> result;
 
-	// write your code here
+	trie t = build_trie(patterns);
+	result = trie_matching(text,t);
 
 	return result;
 }
 
 int main (void)
 {
-	string t;
+	string text;
 	cin >> text;
 
 	int n;
@@ -61,7 +104,7 @@ int main (void)
 	}
 
 	vector <int> ans;
-	ans = solve (t, n, patterns);
+	ans = solve (text, n, patterns);
 
 	for (int i = 0; i < (int) ans.size (); i++)
 	{
