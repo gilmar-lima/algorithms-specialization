33cca921f82b6a45553358fabce39540f58e9b84
Gilmar Lima <gilmar.mail@gmail.com>
2021-03-18 07:32:05 -0300

    inverse first

--- algorithms-on-strings/week2/bwtinverse/bwtinverse.cpp
+++ algorithms-on-strings/week2/bwtinverse/bwtinverse.cpp
@@ -11,8 +11,18 @@ using std::vector;
 
 string InverseBWT(const string& bwt) {
   string text = "";
+  string sample (bwt.size(),'x');
+  vector<string> M (bwt.size(), sample);
 
-  // write your code here
+  for (size_t i = 0; i < bwt.size(); i++)
+  {
+    for (auto &&str : M) rotate(str.begin(), str.begin()+str.size()-1, str.end());
+    for (size_t j = 0; j < bwt.size(); j++) M[j][0] = bwt[j];    
+    sort(M.begin(), M.end());    
+  }
+
+  text = M.front();
+  rotate(text.begin(), text.begin()+1, text.end());
 
   return text;
 }
