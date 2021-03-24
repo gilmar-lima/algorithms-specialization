e74431e952691b0699a5bd6d103628e92f5011c3
Gilmar Lima <gilmar.mail@gmail.com>
2021-03-23 23:27:30 -0300

    bwtinverse second

--- algorithms-on-strings/week2/bwtinverse/bwtinverse.cpp
+++ algorithms-on-strings/week2/bwtinverse/bwtinverse.cpp
@@ -2,30 +2,73 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unordered_map>
 
-using std::cin;
-using std::cout;
-using std::endl;
-using std::string;
-using std::vector;
+using namespace std;
+
+vector<string> parse_column(const string& text){
+
+  unordered_map<char,int> occurrence;
+  vector<string> column (text.size(),"#");
+
+  for (size_t i = 0; i < text.size(); i++)
+  {
+    int count  = 1;
+    if(occurrence.count(text[i]) != 0){
+      count = occurrence.find(text[i])->second;
+      count++;           
+    }
+    occurrence[text[i]] = count;
+    column.push_back(text[i] + to_string(count));
+  }
+  
+
+  for (auto &&element : text)
+  {
+    int count  = 1;
+    if(occurrence.count(element) != 0){
+      count = occurrence.find(element)->second;
+      count++;           
+    }
+    occurrence[element] = count;
+    column.push_back(element + to_string(count));
+  }
+
+  return column;  
+}
+
+unordered_map<string,string> map_column(vector<string> column1, vector<string> column2)
+{
+  unordered_map<string,string> mapping;  
+  for (size_t i = 0; i < column1.size(); i++)
+    mapping.insert(pair<string,string> (column1[i],column2[i]));
+  return mapping;  
+} 
 
 string InverseBWT(const string& bwt) {
   string text = "";
-  string sample (bwt.size(),'x');
-  vector<string> M (bwt.size(), sample);
+  string bwt_ordered = bwt;
+  sort(bwt_ordered.begin(), bwt_ordered.end());
 
-  for (size_t i = 0; i < bwt.size(); i++)
+  vector<string> last_column =  parse_column(bwt);
+  vector<string> first_column =  parse_column(bwt_ordered);
+
+  unordered_map<string,string> first_to_last = map_column(first_column,last_column);
+  string first_match = first_column[0];
+    
+  for (size_t i = 0; i < bwt_ordered.size(); i++)
   {
-    for (auto &&str : M) rotate(str.begin(), str.begin()+str.size()-1, str.end());
-    for (size_t j = 0; j < bwt.size(); j++) M[j][0] = bwt[j];    
-    sort(M.begin(), M.end());    
+    text = text + first_match[0];
+    first_match = first_to_last.find(first_match)->second;    
   }
 
-  text = M.front();
-  rotate(text.begin(), text.begin()+1, text.end());
-
+  reverse(text.begin(),text.end());  
+    
   return text;
 }
+
+
+
 
 int main() {
   string bwt;
