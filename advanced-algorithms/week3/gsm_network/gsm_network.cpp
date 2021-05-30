207c222ad86361427e31c037ed7d85ea65ff8a33
Gilmar Lima <gilmar.mail@gmail.com>
2021-05-30 13:44:30 -0300

    index table

--- advanced-algorithms/week3/gsm_network/gsm_network.cpp
+++ advanced-algorithms/week3/gsm_network/gsm_network.cpp
@@ -4,42 +4,60 @@
 
 using namespace std;
 
+typedef vector<vector<int>> int_matrix;
+
+vector<vector<int>> create_index_table(int num_vertices);
+
 struct Edge {
-    int from;
-    int to;
+	int from;
+	int to;
 };
-
 struct ConvertGSMNetworkProblemToSat {
-    int numVertices;
-    vector<Edge> edges;
+	int numVertices;
+	vector<Edge> edges;
 
-    ConvertGSMNetworkProblemToSat(int n, int m) :
-        numVertices(n),
-        edges(m)
-    {  }
+	ConvertGSMNetworkProblemToSat(int n, int m) :
+		numVertices(n),
+		edges(m)
+	{  }
 
-    void printEquisatisfiableSatFormula() {
-        // This solution prints a simple satisfiable formula
-        // and passes about half of the tests.
-        // Change this function to solve the problem.
-        cout << "3 2" << endl;
-        cout << "1 2 0" << endl;
-        cout << "-1 -2 0" << endl;
-        cout << "1 -2 0" << endl;
-    }
+	void printEquisatisfiableSatFormula() {
+
+		int_matrix clauses;
+
+		int_matrix translation_table = create_index_table(numVertices);
+
+
+		
+	}
 };
+
+int_matrix create_index_table(int num_vertices){
+
+	vector<int> row (3,0);
+	int_matrix table (num_vertices,row);
+
+	for (int i = 0, num_variable = 1; i < num_vertices; i++)
+	{
+		for(int j = 0; j < 3; j++){
+			table[i][j] = num_variable;
+			num_variable++;			
+		}
+	}
+	
+}
 
 int main() {
-    ios::sync_with_stdio(false);
+	ios::sync_with_stdio(false);
 
-    int n, m;
-    cin >> n >> m;
-    ConvertGSMNetworkProblemToSat converter(n, m);
-    for (int i = 0; i < m; ++i) {
-        cin >> converter.edges[i].from >> converter.edges[i].to;
-    }
+	int n, m;
+	cin >> n >> m;
+	ConvertGSMNetworkProblemToSat converter(n, m);
+	for (int i = 0; i < m; ++i) {
+		cin >> converter.edges[i].from >> converter.edges[i].to;
+	}
 
-    converter.printEquisatisfiableSatFormula();
+	converter.printEquisatisfiableSatFormula();
 
-    return 0;
+	return 0;
 }
