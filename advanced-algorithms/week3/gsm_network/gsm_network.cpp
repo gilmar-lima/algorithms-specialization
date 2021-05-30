2144a68da73052cf0d95a24f4d266e0ace53f277
Gilmar Lima <gilmar.mail@gmail.com>
2021-05-30 20:24:19 -0300

    clauses

--- advanced-algorithms/week3/gsm_network/gsm_network.cpp
+++ advanced-algorithms/week3/gsm_network/gsm_network.cpp
@@ -6,12 +6,16 @@ using namespace std;
 
 typedef vector<vector<int>> int_matrix;
 
-vector<vector<int>> create_index_table(int num_vertices);
-
+const int NUM_COLORS = 3;
 struct Edge {
 	int from;
 	int to;
 };
+
+vector<vector<int>> create_index_table(int num_vertices);
+int_matrix create_relation_clauses(vector<Edge> edges, int_matrix table);
+int_matrix create_color_clauses(int num_vertices, int_matrix table);
+void print_clauses(int_matrix clauses);
 struct ConvertGSMNetworkProblemToSat {
 	int numVertices;
 	vector<Edge> edges;
@@ -23,27 +27,69 @@ struct ConvertGSMNetworkProblemToSat {
 
 	void printEquisatisfiableSatFormula() {
 
+		int_matrix relation_clauses, color_clauses;
 		int_matrix clauses;
 
 		int_matrix translation_table = create_index_table(numVertices);
+		relation_clauses = create_relation_clauses(edges, translation_table);
+		color_clauses = create_color_clauses(numVertices, translation_table);
 
+		clauses.insert(clauses.begin(),relation_clauses.begin(), relation_clauses.end());
+		clauses.insert(clauses.end(), color_clauses.begin(), color_clauses.end());
 
-		
+		print_clauses(clauses);
 	}
 };
 
+void print_clauses(int_matrix clauses){
+
+	for(auto clause : clauses){
+		for(auto variable : clause){
+			cout << variable << ' ';
+		}
+		cout << endl;
+	}
+}
+
+int_matrix create_color_clauses(int num_vertices, int_matrix table){
+
+	int_matrix clauses;
+	for (size_t vertex = 1; vertex <= num_vertices; vertex++)
+	{
+		clauses.push_back({table[vertex-1][0], -table[vertex-1][1], -table[vertex-1][2]});
+		clauses.push_back({-table[vertex-1][0], table[vertex-1][1], -table[vertex-1][2]});
+		clauses.push_back({-table[vertex-1][0], -table[vertex-1][1], table[vertex-1][2]});
+	}
+	return clauses;
+}
+
+int_matrix create_relation_clauses(vector<Edge> edges, int_matrix table){
+
+	int_matrix clauses;
+
+	for(Edge e : edges){
+		for (size_t color = 1; color <= NUM_COLORS; color++)
+		{
+			clauses.push_back({table[e.from-1][color-1], table[e.to-1][color-1]});
+			clauses.push_back({-table[e.from-1][color-1], -table[e.to-1][color-1]});			
+		}
+	}
+	return clauses;
+}
+
 int_matrix create_index_table(int num_vertices){
 
-	vector<int> row (3,0);
+	vector<int> row (NUM_COLORS,0);
 	int_matrix table (num_vertices,row);
 
 	for (int i = 0, num_variable = 1; i < num_vertices; i++)
 	{
-		for(int j = 0; j < 3; j++){
+		for(int j = 0; j < NUM_COLORS; j++){
 			table[i][j] = num_variable;
 			num_variable++;			
 		}
 	}
+	return table;
 	
 }
 
