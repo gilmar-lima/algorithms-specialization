9bf24d5c8886a466a7f5612091fcb5a61ea72101
Gilmar Lima <gilmar.mail@gmail.com>
2021-06-27 23:38:47 -0300

    sccs

--- advanced-algorithms/week4/circuit_design/circuit_design.cpp
+++ advanced-algorithms/week4/circuit_design/circuit_design.cpp
@@ -13,10 +13,14 @@ struct Clause {
 };
 
 vector<vector<int>> make_implication_graph(vector<Clause> clauses, int numVars);
-stack<int> DFS(vector<vector<int> > &adj);
-void explore(vector<bool> &visited, vector<vector<int> > &adj,const int &v, stack<int> &store);
+stack<int> dfs_stack(vector<vector<int> > &adj);
+void explore_stack(vector<bool> &visited, vector<vector<int> > &adj,const int &v, stack<int> &store);
 int variable_to_vertex(int variable, int numVars);
 vector<vector<int>> reverse_graph(vector<vector<int>> implication_graph);
+void explore_scc(vector<bool> &visited, vector<vector<int> > &reversed, int &v, unordered_set<int> &scc);
+vector<unordered_set<int>> dfs_scc(vector<vector<int>> &reverserd, stack<int> store);
+vector<unordered_set<int>> SCC(vector<vector<int>> implication_graph);
+
 
 struct TwoSatisfiability {
     int numVars;
@@ -31,13 +35,48 @@ struct TwoSatisfiability {
 
         vector<vector<int>> implication_graph;
         implication_graph = make_implication_graph(clauses, numVars);
+				vector<unordered_set<int>> sccs = SCC(implication_graph);
+
     }
 };
 
 vector<unordered_set<int>> SCC(vector<vector<int>> implication_graph){
-	stack<int> store = DFS(implication_graph);
+	stack<int> store = dfs_stack(implication_graph);
 	vector<vector<int>> reversed = reverse_graph(implication_graph);
+	vector<unordered_set<int>> sccs = dfs_scc(reversed, store);
+	return sccs;
+}
 
+void explore_scc(vector<bool> &visited, vector<vector<int> > &reversed, int &v, unordered_set<int> &scc){
+
+  visited[v] = true;
+	scc.insert(v);
+
+  for (auto w : reversed[v])
+  {
+    if(!visited[w]){
+      explore_scc(visited,reversed,w,scc);      
+    }
+  }
+	return;
+}
+
+vector<unordered_set<int>> dfs_scc(vector<vector<int>> &reverserd, stack<int> store){
+  vector<bool> visited (reverserd.size(), false);
+	vector<unordered_set<int>> sccs;
+	unordered_set<int> scc;
+
+  while(!store.empty())
+  {
+    if(visited[store.top()]){
+			store.pop();
+			continue;
+		}
+    explore_scc(visited,reverserd,store.top(), scc);
+		sccs.push_back(scc);
+		scc.clear();
+  } 
+  return sccs;
 }
 
 vector<vector<int>> reverse_graph(vector<vector<int>> implication_graph){
@@ -51,33 +90,32 @@ vector<vector<int>> reverse_graph(vector<vector<int>> implication_graph){
 
 	for(int v = 0; v < implication_graph.size(); v++){
 		reverse_vertex(v);		
-	}	
-
+	}
 	return reversed;
 }
 
-stack<int> DFS(vector<vector<int> > &adj){
+stack<int> dfs_stack(vector<vector<int> > &adj){
   stack<int> store;
   vector<bool> visited (adj.size(), false);
 
   for (size_t v = 0; v < adj.size(); v++)
   {
     if(visited[v]) continue;
-    explore(visited,adj,v, store); 
+    explore_stack(visited,adj,v, store);
   } 
   return store;
 }
 
-void explore(vector<bool> &visited, vector<vector<int> > &adj,const int &v, stack<int> &store){
+void explore_stack(vector<bool> &visited, vector<vector<int> > &adj,const int &v, stack<int> &store){
 
   visited[v] = true;
 
   for (auto w : adj[v])
   {
     if(!visited[w]){
-      explore(visited,adj,w,store);      
+      explore_stack(visited,adj,w,store);      
     }else{
-      store.push(w);
+      store.push(v);
 			return;
     }
   }
