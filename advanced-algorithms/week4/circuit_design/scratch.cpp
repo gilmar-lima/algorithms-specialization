4a5163efc6644a47f5a327bc05d514435ef685fb
Gilmar Lima <gilmar.mail@gmail.com>
2021-06-28 08:21:35 -0300

    correction

--- advanced-algorithms/week4/circuit_design/scratch.cpp
+++ advanced-algorithms/week4/circuit_design/scratch.cpp
@@ -6,39 +6,12 @@
 
 using namespace std;
 
-
-struct Clause {
-    int firstVar;
-    int secondVar;
-};
-
-vector<vector<int>> make_implication_graph(vector<Clause> clauses, int numVars);
 stack<int> dfs_stack(vector<vector<int> > &adj);
 void explore_stack(vector<bool> &visited, vector<vector<int> > &adj,const int &v, stack<int> &store);
 int variable_to_vertex(int variable, int numVars);
 vector<vector<int>> reverse_graph(vector<vector<int>> implication_graph);
 void explore_scc(vector<bool> &visited, vector<vector<int> > &reversed, int &v, unordered_set<int> &scc);
 vector<unordered_set<int>> dfs_scc(vector<vector<int>> &reverserd, stack<int> store);
-vector<unordered_set<int>> SCC(vector<vector<int>> implication_graph);
-
-
-struct TwoSatisfiability {
-    int numVars;
-    vector<Clause> clauses;
-
-    TwoSatisfiability(int n, int m) :
-        numVars(n),
-        clauses(m)
-    {  }
-
-    bool isSatisfiable(vector<int>& result) {
-
-        vector<vector<int>> implication_graph;
-        implication_graph = make_implication_graph(clauses, numVars);
-				vector<unordered_set<int>> sccs = SCC(implication_graph);
-
-    }
-};
 
 vector<unordered_set<int>> SCC(vector<vector<int>> implication_graph){
 	stack<int> store = dfs_stack(implication_graph);
@@ -118,56 +91,15 @@ void explore_stack(vector<bool> &visited, vector<vector<int> > &adj,const int &v
 	store.push(v);
 }
 
-vector<vector<int>> make_implication_graph(vector<Clause> clauses, int numVars){
-    vector<vector<int>> implication_graph (2*numVars);
-
-    for(Clause clause_ : clauses){
-
-        int l1 = variable_to_vertex(clause_.firstVar,numVars);
-        int l2 = variable_to_vertex(clause_.secondVar,numVars);
-        int not_l1 = variable_to_vertex(-clause_.firstVar,numVars);
-        int not_l2 = variable_to_vertex(-clause_.secondVar,numVars);
-
-        implication_graph[not_l1].push_back(l2);
-        implication_graph[not_l2].push_back(l1);
-    }
-    return implication_graph;
-}
-
-int variable_to_vertex(int variable, int numVars){
-
-    if(variable<0) return (abs(variable) - 1 + numVars);
-    return variable -1;    
-}
 
 int main() {
-    ios::sync_with_stdio(false);
-
-    int n, m;
-    cin >> n >> m;
-    TwoSatisfiability twoSat(n, m);
-    for (int i = 0; i < m; ++i) {
-        cin >> twoSat.clauses[i].firstVar >> twoSat.clauses[i].secondVar;
-    }
-
-    vector<int> result(n);
-    if (twoSat.isSatisfiable(result)) {
-        cout << "SATISFIABLE" << endl;
-        for (int i = 1; i <= n; ++i) {
-            if (result[i-1]) {
-                cout << -i;
-            } else {
-                cout << i;
-            }
-            if (i < n) {
-                cout << " ";
-            } else {
-                cout << endl;
-            }
-        }
-    } else {
-        cout << "UNSATISFIABLE" << endl;
-    }
-
-    return 0;
-}
+  size_t n, m;
+  std::cin >> n >> m;
+  vector<vector<int> > adj(n, vector<int>());
+  for (size_t i = 0; i < m; i++) {
+    int x, y;
+    std::cin >> x >> y;
+    adj[x].push_back(y);
+  }
+  SCC(adj);
+}       
